b58dcf7b52d4b1f4a9ed1d11e773fcfba34eae53
szymonszoldra <szymon.szoldra@gmail.com>
2021-02-12 21:47:02 +0100

    Remove / from links

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 if ('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/serviceWorker.js')
+  navigator.serviceWorker.register('serviceWorker.js')
     .then(reg => console.log('Service worker registered!', reg))
     .catch(err => console.log('Failed!', err));
 }
 
 const icons = [
-  '/assets/postgre.png',
-  '/assets/python.png',
-  '/assets/react.png',
-  '/assets/next.png',
-  '/assets/redux.png',
-  '/assets/js.png',
-  '/assets/docker.png',
-  '/assets/gatsby.png',
-  '/assets/typescript.png',
-  '/assets/github.png',
-  '/assets/serverless.png',
-  '/assets/css.png',
-  '/assets/jira.png',
-  '/assets/html5.png',
-  '/assets/terminal.png',
-  '/assets/git.png',
-  '/assets/graphql.png',
-  '/assets/sass.png',
-  '/assets/jwt.png',
-  '/assets/aws.png',
-  '/assets/linux.png',
-  '/assets/webpack.png',
-  '/assets/mongodb.png',
-  '/assets/arch.png',
-  '/assets/express.png',
-  '/assets/redis.png',
-  '/assets/node-js.png',
-  '/assets/firebase.png'
+  'assets/postgre.png',
+  'assets/python.png',
+  'assets/react.png',
+  'assets/next.png',
+  'assets/redux.png',
+  'assets/js.png',
+  'assets/docker.png',
+  'assets/gatsby.png',
+  'assets/typescript.png',
+  'assets/github.png',
+  'assets/serverless.png',
+  'assets/css.png',
+  'assets/jira.png',
+  'assets/html5.png',
+  'assets/terminal.png',
+  'assets/git.png',
+  'assets/graphql.png',
+  'assets/sass.png',
+  'assets/jwt.png',
+  'assets/aws.png',
+  'assets/linux.png',
+  'assets/webpack.png',
+  'assets/mongodb.png',
+  'assets/arch.png',
+  'assets/express.png',
+  'assets/redis.png',
+  'assets/node-js.png',
+  'assets/firebase.png'
 ];
 
 const fragment = document.createDocumentFragment();
--- serviceWorker.js
+++ serviceWorker.js
@@ -2,42 +2,42 @@ const STATIC_CACHE = 'v1';
 const DYNAMIC_CACHE = 'dv1';
 
 const icons = [
-  '/assets/postgre.png',
-  '/assets/python.png',
-  '/assets/react.png',
-  '/assets/next.png',
-  '/assets/redux.png',
-  '/assets/js.png',
-  '/assets/docker.png',
-  '/assets/gatsby.png',
-  '/assets/typescript.png',
-  '/assets/github.png',
-  '/assets/serverless.png',
-  '/assets/css.png',
-  '/assets/jira.png',
-  '/assets/html5.png',
-  '/assets/terminal.png',
-  '/assets/git.png',
-  '/assets/graphql.png',
-  '/assets/sass.png',
-  '/assets/jwt.png',
-  '/assets/aws.png',
-  '/assets/linux.png',
-  '/assets/webpack.png',
-  '/assets/mongodb.png',
-  '/assets/arch.png',
-  '/assets/express.png',
-  '/assets/redis.png',
-  '/assets/node-js.png',
-  '/assets/firebase.png'
+  'assets/postgre.png',
+  'assets/python.png',
+  'assets/react.png',
+  'assets/next.png',
+  'assets/redux.png',
+  'assets/js.png',
+  'assets/docker.png',
+  'assets/gatsby.png',
+  'assets/typescript.png',
+  'assets/github.png',
+  'assets/serverless.png',
+  'assets/css.png',
+  'assets/jira.png',
+  'assets/html5.png',
+  'assets/terminal.png',
+  'assets/git.png',
+  'assets/graphql.png',
+  'assets/sass.png',
+  'assets/jwt.png',
+  'assets/aws.png',
+  'assets/linux.png',
+  'assets/webpack.png',
+  'assets/mongodb.png',
+  'assets/arch.png',
+  'assets/express.png',
+  'assets/redis.png',
+  'assets/node-js.png',
+  'assets/firebase.png'
 ];
 
 const filesToChache = [
-  '/',
-  '/index.html',
-  '/main.js',
-  '/style.css',
-  '/pages/offline.html',
+  '',
+  'index.html',
+  'main.js',
+  'style.css',
+  'pages/offline.html',
   'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap',
   ...icons
 ];
@@ -81,7 +81,7 @@ self.addEventListener('fetch', (event) => {
         )
       }).catch(() => {
         const isNotAnIcon = event.request.url.indexOf('.html') !== -1;
-        if (isNotAnIcon) return caches.match('./pages/offline.html');
+        if (isNotAnIcon) return caches.match('.pages/offline.html');
       })
   );
 });
